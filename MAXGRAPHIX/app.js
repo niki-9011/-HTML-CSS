a83a5707768b6422746f56e17892bd68c6effb7d
Nikolay Videnov <niki901115@gmail.com>
2024-06-02 21:44:28 +0300

    contact form adding

--- MAXGRAPHIX/app.js
+++ MAXGRAPHIX/app.js
@@ -6,3 +6,20 @@ function myFunction() {
         x.style.display = "block";
     }
 }
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function showContactForm() {
+    document.getElementById("contactForm").style.display = "block";
+}
+
+function closeContactForm() {
+    document.getElementById("contactForm").style.display = "none";
+}
